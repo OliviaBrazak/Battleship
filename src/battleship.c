b5af57ad764304a18aab898daec02c663fd36566
Saucetray <justinsostreofficial@gmail.com>
2019-03-29 20:29:23 -0400

    Comments

--- src/battleship.c
+++ src/battleship.c
@@ -1,3 +1,11 @@
+///
+/// FILE: battleship.c
+/// DESCRIPTION: battleship client
+/// AUTHOR: Justin Sostre
+/// CONTRIBUTORS: Olivia Brazak
+///
+
+
 int main() {
     return 1;
 }
--- src/battleship_engine.c
+++ src/battleship_engine.c
@@ -12,14 +12,17 @@
 typedef int64_t bitboard;
 
 struct Battle_Board {
-    bitboard p1_ships;
-    bitboard p2_ships;
-    bitboard p1_hits;
-    bitboard p2_hits;
+        bitboard p1_ships;
+        bitboard p2_ships;
+        bitboard p1_hits;
+        bitboard p2_hits;
 };
 
 typedef struct Battle_Board Board;
 
+/// create_board - initializes a blank board
+///
+/// return - returns the pointer to the new board
 Board* create_board() {
 	Board* board = malloc(sizeof(board));
 	board->p1_ships = 0;
@@ -29,8 +32,25 @@ Board* create_board() {
 	return board;
 }
 
-int place_ship(int board_type, short rot, short player, short x, short y) {
-	if (players == 1) {
+/// place_ship - places the ship
+/// arguments
+/// board_type: short - how many spaces the battleship takes up
+/// rot: short - what rotation | 1-90* | 2-180*
+/// player: short - player number
+/// x: short - x-coordinate to start placing the ship
+/// y: short - y-coordinate to start placing the ship
+/// return - returns whether you can place or not
+int place_ship(short board_type, short rot, short player, short x, short y) {
+        return 1;   
+}
 
 
+int place_ship_validation(short board_type, short rot, 
+        short player, short x, short y) {
+        return 1;
+}
+
+int fire_cannon(short target, short x, short y) {
+        return 1;
+}
 	
